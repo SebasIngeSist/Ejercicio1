1589bfb34582832bde37a2fbdd568de68ca363e8
Sebas <sebastianporras510@mail.com>
2025-04-10 11:53:16 -0500

    Subiendo ejercicio 1

---
+++ Programa/Ejercicio/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ejercicio1();
+        }
+
+        static void ejercicio1()
+        {
+
+            string nombre;
+
+            Console.Write("Ingresa su nombre: ");
+            nombre = Console.ReadLine();
+
+            Console.WriteLine(nombre + " Bienvenido al curso de fundamentos");
+        }
+
+    }
+}
